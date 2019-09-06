c418ae92a55c53a99c658697480235d8c3e3a89e
Naoaki Obiki <obiki.wk@gmail.com>
2019-09-06 22:49:49 +0900

    :+1: fix user group

--- provision.sh
+++ provision.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+export COMPOSE_VER=1.24.1
+
 apt update
 apt install -y vim \
   git \
+  locales \
+  tzdata \
   sed \
   gawk \
   curl \
-  wget
+  wget \
+  dumb-init
 
-
-COMPOSE_VER=1.24.1
+# langage & timezone
+locale-gen ja_JP.UTF-8
+localedef -f UTF-8 -i ja_JP ja_JP.UTF-8
+update-locale LANG=ja_JP.UTF-8
+ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 
 # docker-ce
-sudo apt-get install -y apt-transport-https ca-certificates curl wget gnupg2 software-properties-common lsb-release
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y wget docker-ce
+apt-get install -y apt-transport-https ca-certificates curl wget gnupg2 software-properties-common lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y wget docker-ce
 
 # docker-compose
-sudo wget https://github.com/docker/compose/releases/download/${COMPOSE_VER}/docker-compose-Linux-x86_64 -O /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo gpasswd -a ${USER} docker
+wget https://github.com/docker/compose/releases/download/${COMPOSE_VER}/docker-compose-Linux-x86_64 -O /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+gpasswd -a vagrant docker
 
